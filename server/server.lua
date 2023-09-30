64ffdd66a657a8974f7a48378a7b25e4b1bc6193
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 13:42:03 +0100

    version checker

--- server/server.lua
+++ server/server.lua
@@ -1,6 +1,37 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 local PlantsLoaded = false
 
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-indiantobacco/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
+
 -- use seed
 RSGCore.Functions.CreateUseableItem("indtobaccoseed", function(source, item)
     local src = source
@@ -396,3 +427,8 @@ AddEventHandler('rsg-indiantobacco:server:removeitem', function(item, amount)
     Player.Functions.RemoveItem(item, amount)
     TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items[item], "remove")
 end)
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
